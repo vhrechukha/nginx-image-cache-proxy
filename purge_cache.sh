f93c74b71a69a0e6c1b042c6717ff3e4e7c0e971
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2024-08-19 13:22:24 +0200

    Fixed cache purge

--- purge_cache.sh
+++ purge_cache.sh
@@ -1,20 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
-CACHE_PATH="/var/cache/nginx/img_cache"
+CACHE_PATH="/var/cache/nginx/image_cache"
 
 if [ -z "$1" ]; then
-  echo "Usage: ./purge_cache.sh <file_path>"
+  echo "Usage: $0 <file_path>"
   exit 1
 fi
 
 FILE_PATH=$1
-CACHE_KEY=$(echo -n "${FILE_PATH}" | md5sum | awk '{print $1}')
-CACHE_FILE=$(find $CACHE_PATH -type f -name "*${CACHE_KEY}*")
 
-if [ -n "$CACHE_FILE" ]; then
-  echo "Purging cache for ${FILE_PATH}..."
+
+CACHE_KEY=$(echo -n "$FILE_PATH" | md5sum | awk '{print $1}')
+echo "DEBUG: CACHE_KEY is '$CACHE_KEY'"
+
+CACHE_FILE="${CACHE_PATH}/${CACHE_KEY}"
+
+# Debugging output
+echo "DEBUG: CACHE_FILE path is '$CACHE_FILE'"
+
+if [ -f "$CACHE_FILE" ]; then
+  echo "Cache file found: $CACHE_FILE"
   rm -f $CACHE_FILE
-  echo "Cache purged for ${FILE_PATH}."
+  echo "Cache file purged."
 else
-  echo "No cache found for ${FILE_PATH}."
+  echo "No cache file found for ${FILE_PATH}"
+  echo "Cache path: $CACHE_PATH"
+  echo "CACHE_KEY: $CACHE_KEY"
+
+  # List all files in the cache directory and its subdirectories
+  echo "Listing all cache files in $CACHE_PATH:"
+  find "$CACHE_PATH" -type f -print
 fi
